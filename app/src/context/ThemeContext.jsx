f2aab5751fe7d630da2a893d871c61c203ad9757
PriyavarshiniRavikumar <varsharkg1@gmail.com>
2023-11-02 22:05:18 +0530

    ThemeContext

---
+++ app/src/context/ThemeContext.jsx
@@ -0,0 +1,7 @@
+// JavaScript source code
+import React, { createContext, useState } from "react";
+
+const ThemeContext = createContext(undefined);
+const ThemeDispatchContext = createContext(undefined);
+const UserContext = createContext(undefined);
+const UserDispatchContext = createContext(undefined);
