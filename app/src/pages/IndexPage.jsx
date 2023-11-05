feccedfbaaa81fe765c9f46e195861e5987643e5
PriyavarshiniRavikumar <varsharkg1@gmail.com>
2023-11-05 20:51:48 +0530

    use context  and use state

--- app/src/pages/IndexPage.jsx
+++ app/src/pages/IndexPage.jsx
@@ -6,3 +6,10 @@ import { ThemeContext } from "../context/ThemeContext";
 import { UserContext } from "../context/ThemeContext";
 import { UserDispatchContext } from "../context/ThemeContext";
 
+const IndexPage = () => {
+    const themeContext = useContext(ThemeContext);
+    const userDispatch = useContext(UserDispatchContext);
+    const userContext = useContext(UserContext);
+    const [nameInput, setnameInput] = useState("");
+    const [imageInput, setImageInput] = useState("");
+
