f1e4da2083379d9da14a26887cd2ffabe06ad080
PriyavarshiniRavikumar <varsharkg1@gmail.com>
2023-11-08 22:08:08 +0530

    App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -8,3 +8,16 @@ import { ThemeProvider } from "./context/ThemeContext";
 import { UserProvider } from "./context/ThemeContext";
 
 import IndexPage from "./pages/IndexPage";
+function App() {
+    return (
+        <>
+            <ThemeProvider>
+                <UserProvider>
+                    <IndexPage />
+                </UserProvider>
+            </ThemeProvider>
+        </>
+    );
+}
+
+export default App;
